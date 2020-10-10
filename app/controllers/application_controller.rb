bee2d52c2ba74bedadf73e91dd3ebbcb1a81887a
Lucas Ethan <asmrit1010@gmail.com>
2020-10-10 11:18:21 +0200

    Added hello and goodbye routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  def hello
+    render html: "<h1>hello world</h1>".html_safe
+  end
+
+  def goodbye
+    render html: "<h1>goodbye world</h1>".html_safe
+  end
 end
