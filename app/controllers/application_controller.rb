3ee13a515b4d44d5bdab035d75efe6e72dbb458f
Lucas Ethan <asmrit1010@gmail.com>
2020-10-17 09:44:29 +0200

    Added static pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,2 @@
 class ApplicationController < ActionController::Base
-  def hello
-    render html: "<h1>hello world</h1>".html_safe
-  end
-
-  def goodbye
-    render html: "<h1>goodbye world</h1>".html_safe
-  end
 end
