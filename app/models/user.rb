2aa2bad7c1bc5cad182fffeb50b3838c82fcf838
Lucas Ethan <asmrit1010@gmail.com>
2020-10-21 13:20:28 +0200

    Added posts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_many :posts
   validates :name, :email, presence: true, length: { minimum: 10 }
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
 end
